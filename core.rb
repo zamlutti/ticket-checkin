02d6af7f5d4e62762bde32d1a17350192cde9adf
Thiago Ganzarolli <tganzarolli@Thiago-Ganzarollis-MacBook-Pro.local>
2011-04-21 04:32:05 -0300

    autorizando

--- core.rb
+++ core.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'open-uri'
 require 'hpricot'
 require 'expense'
+require 'oauth'
 
 get '/ticket_history' do
   number = params[:number]
@@ -28,24 +29,51 @@ get '/ticket_history' do
   doc  
 end
 
+def client
+  OAuth::Consumer.new( "hxGukWgs3XLL1xU4tkrvPv7Fx11j5m0ZXtiyuGOv2Vo~","gAKi8cdPDF10VutPGhQsFzOrlAc~", {
+      :site => "http://api.apontador.com.br", :http_method => :get, :scheme => :query_string, :request_token_path => '/v1/oauth/request_token', :authorize_path => '/v1/oauth/authorize', :access_token_path => '/v1/oauth/access_token'
+      })
+end
+
+get '/auth/apontador' do
+  @request_token=client.get_request_token(:oauth_callback => redirect_uri)
+  session[:request_token]=@request_token
+  redirect @request_token.authorize_url
+  
+end
+
+get '/apontador_callback' do
+  @request_token = session[:request_token]
+  @access_token=@request_token.get_access_token
+end
+
+
 private
   def get_doc operation, number
-  url = "http://www.ticket.com.br/portal/portalcorporativo/dpages/service/consulteseusaldo/seeBalance.jsp?txtOperation=#{operation}&txtCardNumber=#{number}"
-  begin
-    puts url
-    f = open(url)
-    #f = File.open('mock.txt')
-    doc = Hpricot(Iconv.conv('UTF-8', f.charset, f.read))
-    #doc = Hpricot(f)
-    doc = doc.at("body/script").inner_html
-    initial_index = doc =~ /\[/
-    final_index = doc =~ /\]/
-    doc = doc[initial_index..final_index]
-    doc = doc.gsub("descricao", "\"descricao\"")
-    doc = doc.gsub("data", "\"data\"")
-    doc = doc.gsub("valor", "\"valor\"")
-    doc = doc.gsub("'", "\"")
-  rescue => e
-    return [503,"Erro: #{e}"]
+    url = "http://www.ticket.com.br/portal/portalcorporativo/dpages/service/consulteseusaldo/seeBalance.jsp?txtOperation=#{operation}&txtCardNumber=#{number}"
+    begin
+      puts url
+      f = open(url)
+      #f = File.open('mock.txt')
+      doc = Hpricot(Iconv.conv('UTF-8', f.charset, f.read))
+      #doc = Hpricot(f)
+      doc = doc.at("body/script").inner_html
+      initial_index = doc =~ /\[/
+      final_index = doc =~ /\]/
+      doc = doc[initial_index..final_index]
+      doc = doc.gsub("descricao", "\"descricao\"")
+      doc = doc.gsub("data", "\"data\"")
+      doc = doc.gsub("valor", "\"valor\"")
+      doc = doc.gsub("'", "\"")
+    rescue => e
+      return [503,"Erro: #{e}"]
+    end
   end
-end
+
+
+  def redirect_uri
+    uri = URI.parse(request.url)
+    uri.path = '/apontador_callback'
+    uri.query = nil
+    uri.to_s
+  end
