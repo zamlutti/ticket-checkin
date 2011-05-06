209d91f8b0885a2be06d27ac007cf8bde9dbf1b9
tganzarolli <thiago.ganzarolli@lbslocal.com>
2011-05-06 14:31:42 -0300

    vale => visa e deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export target='../deployment/ticket-checkin'
+mv $target'/.git' $target'/.git_tmp'
+cp -R ./ $target
+rm -rf $target'/.git'
+mv $target'/.git_tmp' $target'/.git'
+rm $target'/.gitignore'
+rm $target/'Gemfile'
+rm $target/'Gemfile.lock'
+cd $target
+ls -la
+git commit -m 'deploy'
+git push origin master
+
